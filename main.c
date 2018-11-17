c5a3a2cc32dbd431cd3fded621c1501b43beb6e3
bcozic <bcozic@student.42.fr>
2018-11-17 18:02:07 +0100

    thread safe reallocf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bcozic <bcozic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 22:39:26 by bcozic            #+#    #+#             */
-/*   Updated: 2018/11/16 19:33:35 by bcozic           ###   ########.fr       */
+/*   Updated: 2018/11/17 14:49:19 by bcozic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,91 +16,84 @@
 #include <unistd.h>
 
 
-// static void	*multi_malloc(void* ret)
-// {
-// 	(void)ret;
-// 	int i = 1000;
-// 	char *test;
-// 	char *test2;
-// 	char *test3;
-// 	char *test4;
-// 	char *test5;
-// 	char *test6;
+static void	*multi_malloc(void* ret)
+{
+	(void)ret;
+	int i = 1000;
+	char *test;
+	char *test2;
+	char *test3;
+	char *test4;
+	char *test5;
+	char *test6;
 
-// 	while(i--)
-// 	{
-// 		if (!(test = valloc(10)))
-// 		{
-// 			ft_printf("ERROR\n");
-// 			return (ret);
-// 		}
-// 		if ((size_t)test % 4096)
-// 			ft_printf("test1 fail\n");
-// 		if (!(test2 = valloc(100)))
-// 		{
-// 			ft_printf("ERROR\n");
-// 			return (ret);
-// 		}
-// 		if ((size_t)test2 % 4096)
-// 			ft_printf("test2 fail\n");
-// 		if (!(test3 = malloc(20)))
-// 		{
-// 			ft_printf("ERROR\n");
-// 			return (ret);
-// 		}
-// 		if (!(test4 = malloc(48)))
-// 		{
-// 			ft_printf("ERROR\n");
-// 			return (ret);
-// 		}
-// 		if (!(test5 = valloc(600)))
-// 		{
-// 			ft_printf("ERROR\n");
-// 			return (ret);
-// 		}
-// 		if ((size_t)test5 % 4096)
-// 			ft_printf("test5 fail\n");
-// 		if (!(test6 = malloc(8000)))
-// 		{
-// 			ft_printf("ERROR\n");
-// 			return (ret);
-// 		}
-// 		free(test4);
-// 		free(test2);
-// 		free(test);
-// 		free(test3);
-// 		free(test6);
-// 		free(test5);
-// 	}
-// 	return (ret);
-// }
+	while(i--)
+	{
+		if (!(test = malloc(10)))
+		{
+			ft_printf("ERROR\n");
+			return (ret);
+		}
+		if (!(test2 = malloc(100)))
+		{
+			ft_printf("ERROR\n");
+			return (ret);
+		}
+		if (!(test3 = reallocf(test, 20)))
+		{
+			ft_printf("ERROR\n");
+			return (ret);
+		}
+		if (!(test4 = reallocf(test2, 48)))
+		{
+			ft_printf("ERROR\n");
+			return (ret);
+		}
+		if (!(test5 = valloc(600)))
+		{
+			ft_printf("ERROR\n");
+			return (ret);
+		}
+		if ((size_t)test5 % 4096)
+			ft_printf("test5 fail\n");
+		if (!(test6 = reallocf(test5, 8000)))
+		{
+			ft_printf("ERROR\n");
+			return (ret);
+		}
+		free(test4);
+		free(test3);
+		free(test6);
+	}
+	return (ret);
+}
 // malloc ./a.out  14.24s user 2.76s system 182% cpu 9.340 total
 // ft_malloc ./a.out  16.07s user 7.05s system 212% cpu 10.856 total
 int	main()
 {
 
 	ft_putstr("begin main\n");
-	// int i = 100;
-	// pthread_t	thread[100];
-	// void		*value[100];
-	// int nb[100];
-	// while(i--)
-	// {
-	// 	nb[i] = i;
-	// 	if (pthread_create(&thread[i], NULL, &multi_malloc, &nb[i]))
-	// 		break ;
-	// 	//ft_printf("new thread %d\n", i);
-	// }
-	// i = 100;
-	// while(i--)
-	// 	pthread_join(thread[i], &value[i]);
+	int i = 100;
+	pthread_t	thread[100];
+	void		*value[100];
+	int nb[100];
+	while(i--)
+	{
+		nb[i] = i;
+		if (pthread_create(&thread[i], NULL, &multi_malloc, &nb[i]))
+			break ;
+		//ft_printf("new thread %d\n", i);
+	}
+	i = 100;
+	while(i--)
+		pthread_join(thread[i], &value[i]);
 
-		char *test;
-		test = malloc(10);
-		test = realloc(test, 1);
-		ft_putstr("test\n");
-		ft_printf("%p\n", test);
-		//test[0] = 42;
-		free(test);
+		// char *test;
+		// test = malloc(10);
+		// test = realloc(test, 1);
+		// ft_putstr("test\n");
+		// ft_printf("%p\n", test);
+		// //test[0] = 42;
+		// free(test);
 	return(0);
 }
--- src/reallocf.c
+++ src/reallocf.c
@@ -6,11 +6,31 @@
 /*   By: bcozic <bcozic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 19:41:37 by bcozic            #+#    #+#             */
-/*   Updated: 2018/11/16 21:43:26 by bcozic           ###   ########.fr       */
+/*   Updated: 2018/11/17 14:48:26 by bcozic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_libc.h"
+
+static void		*no_thread_safe_malloc(size_t size)
+{
+	void	*ptr;
+
+	if (size == 0)
+		return (0);
+	if (g_data == NULL)
+		if (!init_data((size_t)getpagesize()))
+			return (0);
+	if (size <= g_data->pages_size)
+		ptr = get_alloc(size, &g_data->tiny,
+				(g_data->pages_size * 100 / g_data->pages_size));
+	else if (size <= g_data->pages_size * 4)
+		ptr = get_alloc(size, &g_data->small,
+				(g_data->pages_size * 4 * 100 / g_data->pages_size));
+	else
+		ptr = get_alloc(size, &g_data->large, 0);
+	return (ptr);
+}
 
 void			*reallocf(void *ptr, size_t size)
 {
@@ -33,9 +53,9 @@ void			*reallocf(void *ptr, size_t size)
 	}
 	if (size == 0)
 		size = 1;
-	pthread_mutex_unlock(&g_mutex);
-	new_ptr = malloc(size);
+	new_ptr = no_thread_safe_malloc(size);
 	if (new_ptr && new_ptr != ptr && original_size)
 		ft_memmove(new_ptr, ptr, original_size);
+	pthread_mutex_unlock(&g_mutex);
 	return (new_ptr);
 }
